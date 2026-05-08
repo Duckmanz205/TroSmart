16ecb9e2db8034e3e78186b61e4330d634a5aa9d
Duckmanz <duc249205@gmail.com>
2026-05-08 13:44:08 +0700

    add project API

---
+++ PhongTroAPI/PhongTroAPI/Entities/HinhAnhPhong.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PhongTroAPI.Entities;
+
+public partial class HinhAnhPhong
+{
+    public int MaAnh { get; set; }
+
+    public int MaPhong { get; set; }
+
+    public string? UrlAnh { get; set; }
+
+    public virtual Phong MaPhongNavigation { get; set; } = null!;
+}
